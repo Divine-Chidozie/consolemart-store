9cc24c6ccd26656cd8f390b1e8a6b3c72ed670ac
Divine Chidozie <igwedivine92@gmail.com>
2025-10-19 01:08:35 +0100

    second changes on input-form

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -50,8 +50,8 @@ const Signin = () => {
   };
 
   return (
-    <section className="w-full md:w-2/5 my-24 mx-auto p-10 text-center">
-      <div className="flex flex-col justify-center items-center mb-5">
+    <section className="w-full md:w-2/5 my-24 mx-auto p-10 text-center shadow-lg drop-shadow-2xl shadow-slate-300">
+      <div className="flex flex-col justify-center items-center mb-5 ">
         <h1 className="font-bold text-2xl">Sign in</h1>
         <p className="font-normal mt-2">
           New user?{" "}
@@ -70,7 +70,7 @@ const Signin = () => {
           value={email}
           onChange={handleEmailChange}
           placeholder="✉️ Email Address"
-          className="border-2 border-gray-200 py-2 pl-2 w-3/5 text-lg text-gray-700 outline-none mb-1"
+          className="w-[80%] border-2 border-gray-200 py-2 pl-2 md:w-3/5 text-lg text-gray-700 outline-none mb-1"
         />
         {emailError && (
           <p className="text-red-600 text-sm mb-3 font-bold">{emailError}</p>
@@ -80,8 +80,8 @@ const Signin = () => {
           type="password"
           value={password}
           onChange={handlePasswordChange}
-          placeholder="🔒 Password"
-          className="border-2 border-gray-200 mb-1 py-2 pl-2 w-3/5 text-lg text-gray-700 outline-none"
+          placeholder="  🔒 Password"
+          className="w-[80%] border-2 border-gray-200 mb-1 py-2 pl-2md:w-3/5 text-lg text-gray-700 outline-none"
         />
         {passwordError && (
           <p className="text-red-600 text-sm mb-3 font-bold">{passwordError}</p>
